1646c1d11b8336545a265a49cb322a5eb6988c15
Insanitium <francis.nadeau@hotmail.ca>
2020-02-06 15:11:34 -0500

    Added a variable

--- Dark scrollbar/userChrome/custom_scrollbar.uc.js
+++ Dark scrollbar/userChrome/custom_scrollbar.uc.js
@@ -16,52 +16,57 @@
         enabled = true;
     }
 
-    var css = '\
-    @namespace url(http: //www.mozilla.org/keymaster/gatekeeper/there.is.only.xul);\
-    scrollbar, scrollbar *, scrollcorner {\
-        -moz-appearance: none !important;\
-        --scrollbar-bg-color: rgb(12,12,13);\
-        --scrollbar-corner-color: rgb(39, 39, 43);\
-        --scrollbar-thumb-color: hsla(0,0%,70%,.4);\
-        --scrollbar-thumb-hover-color: rgba(249,249,250,.4);\
-        --scrollbar-thumb-active-color: rgba(249,249,250,.5);\
-        --scrollbar-thumb-radius: 0px;\
-        --scrollbar-border: 0px solid rgb(12,12,13);\
-        --scrollbar-width: 14px;\
-        --scrollbar-height: var(--scrollbar-width);\
-    }\
-    scrollbar {\
-        background: var(--scrollbar-bg-color) !important;\
-    }\
-    scrollcorner {\
-        background: var(--scrollbar-corner-color) !important;\
-    }\
-    scrollbar[orient="vertical"] {\
-        width: var(--scrollbar-width) !important;\
-        min-width: var(--scrollbar-width) !important;\
-    }\
-    scrollbar[orient="horizontal"] {\
-        height: var(--scrollbar-height) !important;\
-        min-height: var(--scrollbar-height) !important;\
-    }\
-    scrollbar thumb {\
-        background: var(--scrollbar-thumb-color) !important;\
-        border-radius: var(--scrollbar-thumb-radius) !important;\
-    }\
-    scrollbar thumb:hover{\
-        background: var(--scrollbar-thumb-hover-color) !important;\
-    }\
-    scrollbar thumb:active {\
-        background: var(--scrollbar-thumb-active-color) !important;\
-    }\
-    scrollbar thumb[orient="vertical"] {\
-        border-left: var(--scrollbar-border) !important;\
-        border-right: var(--scrollbar-border) !important;\
-    }\
-    scrollbar thumb[orient="horizontal"] {\
-        border-top: var(--scrollbar-border) !important;\
-        border-bottom: var(--scrollbar-border) !important;\
-    }';
+    var css = `
+    @namespace url(http: //www.mozilla.org/keymaster/gatekeeper/there.is.only.xul);
+    scrollbar, scrollbar *, scrollcorner, scrollbarbutton {
+        -moz-appearance: none !important;
+        --scrollbar-bg-color: #212121;
+        --scrollbar-corner-color: #c8c8c8;
+        --scrollbar-thumb-color: #c8c8c8;
+        --scrollbar-thumb-hover-color: #98a3a6;
+        --scrollbar-thumb-active-color: #98a3a6;
+        --scrollbar-thumb-radius: 9px;
+        --scrollbar-border: 6px solid #212121;
+        --scrollbar-border-perpendicular: 6px solid #212121;
+        --scrollbar-width: 18px;
+        --scrollbar-height: var(--scrollbar-width);
+    }
+    scrollbar {
+        background: var(--scrollbar-bg-color) !important;
+    }
+    scrollcorner {
+        background: var(--scrollbar-corner-color) !important;
+    }
+    scrollbar[orient="vertical"] {
+        width: var(--scrollbar-width) !important;
+        min-width: var(--scrollbar-width) !important;
+    }
+    scrollbar[orient="horizontal"] {
+        height: var(--scrollbar-height) !important;
+        min-height: var(--scrollbar-height) !important;
+    }
+    scrollbar thumb {
+        background: var(--scrollbar-thumb-color) !important;
+        border-radius: var(--scrollbar-thumb-radius) !important;
+    }
+    scrollbar thumb:hover{
+        background: var(--scrollbar-thumb-hover-color) !important;
+    }
+    scrollbar thumb:active {
+        background: var(--scrollbar-thumb-active-color) !important;
+    }
+    scrollbar thumb[orient="vertical"] {
+        border-left: var(--scrollbar-border) !important;
+        border-right: var(--scrollbar-border) !important;
+        border-top: var(--scrollbar-border-perpendicular) !important;
+        border-bottom: var(--scrollbar-border-perpendicular) !important;
+    }
+    scrollbar thumb[orient="horizontal"] {
+        border-top: var(--scrollbar-border) !important;
+        border-bottom: var(--scrollbar-border) !important;
+        border-left: var(--scrollbar-border-perpendicular) !important;
+        border-right: var(--scrollbar-border-perpendicular) !important;
+    }`;
 
     var sss = Cc['@mozilla.org/content/style-sheet-service;1'].getService(Ci.nsIStyleSheetService);
     var uri = makeURI('data:text/css;charset=UTF=8,' + encodeURIComponent(css));
